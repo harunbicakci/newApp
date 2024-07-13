e61c77f3fb6e6b0da230b4c135ae0d436d2b5b2d
Mad Triathlete <h_bicakci@msn.com>
2024-07-13 13:25:54 -0400

    sign in and sign up page done

---
+++ newProject/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  newProject
+//
+//  Created by Harun Bicakci on 7/13/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
