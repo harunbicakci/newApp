b51e20bfe0b1b11fd91d9c0653d74d23fc8b15eb
Mad Triathlete <h_bicakci@msn.com>
2024-07-13 22:19:29 -0400

    user object created

---
+++ newProject/Components/SettingsRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsRowView.swift
+//  newProject
+//
+//  Created by Harun Bicakci on 7/13/24.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsRowView()
+}
---
+++ newProject/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  newProject
+//
+//  Created by Harun Bicakci on 7/13/24.
+//
+
+import Foundation
